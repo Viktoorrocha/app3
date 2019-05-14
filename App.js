c2884ad8a58d469756b408f5c926a609faf9b765
Viktor Rocha <viktoorrocha@gmail.com>
2019-05-13 22:34:07 -0400

    Topo

--- App.js
+++ App.js
@@ -7,14 +7,14 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, Image} from 'react-native';
 
 export default class App extends Component{
 
   constructor(props){
     super(props);
 
-    this.state = { escolhaUsuario : '',escolhaComputador : '' }
+    this.state = { escolhaUsuario : '',escolhaComputador : '', resultado: '' }
   }
 
   jokenpo(escolhaUsuario){
@@ -30,16 +30,64 @@ switch(numAleatorio){
   case 2: escolhaComputador = 'tesoura';break;
 }
 
+var resultado = '';
+
+if(escolhaComputador == 'pedra'){
+  if(escolhaUsuario == 'pedra'){
+    resultado = 'empate'
+  }
+
+  if(escolhaUsuario == 'papel'){
+    resultado = 'Usuario ganhou';
+  }
+
+  if(escolhaUsuario == 'tesoura'){
+    resultado = 'computador ganhou';
+  }
+}
+
+if(escolhaComputador == 'papel'){
+  if(escolhaUsuario == 'papel'){
+    resultado = 'empate'
+  }
+
+  if(escolhaUsuario == 'tesoura'){
+    resultado = 'Usuario ganhou';
+  }
+
+  if(escolhaUsuario == 'pedra'){
+    resultado = 'computador ganhou';
+  }
+}
+
+if(escolhaComputador == 'tesoura'){
+  if(escolhaUsuario == 'tesoura'){
+    resultado = 'empate'
+  }
+
+  if(escolhaUsuario == 'pedra'){
+    resultado = 'Usuario ganhou';
+  }
+
+  if(escolhaUsuario == 'papel'){
+    resultado = 'computador ganhou';
+  }
+}
+
    this.setState({ escolhaUsuario : escolhaUsuario,
-                    escolhaComputador : escolhaComputador});
+                    escolhaComputador : escolhaComputador, resultado : resultado});
 
   }
   render(){
     return(
       <View style={{marginTop: 50}}>
+
+     <Topo></Topo>
+      <View></View>
+      <View></View>
         <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
         <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-        <Text>Resultado</Text>
+        <Text>Resultado: </Text>
         <Button title ="Pedra" onPress={() =>  { this.jokenpo('pedra')}}></Button>
         <Button title="Papel" onPress={() => { this.jokenpo('papel')}}></Button>
         <Button title="Tesoura" onPress={() => { this.jokenpo('tesoura')}}></Button>
@@ -47,6 +95,18 @@ switch(numAleatorio){
     );
   }
 }
+
+class Topo extends Component{
+  render(){
+    return(
+      <View>
+      <Image source={require('./imgs/jokenpo.png')}></Image>
+    </View>
+    );
+  }
+}
+
+
   
 
 const styles = StyleSheet.create({
